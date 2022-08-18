fddbddc2b4cbfbc88dfc0ad78c678d6fb417b2bd
veslava <veslavakar@gmail.com>
2022-08-19 00:09:24 +0300

    add header

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -2,7 +2,20 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div className="bg-gradient-to-br from-green-400 to-blue-600 h-16"></div>
+    <div className="flex justify-between items-center bg-gradient-to-br from-green-400 to-blue-600 h-16">
+      <div className="mx-5 text-white font-bold tracking-widest">
+        <a href="#" className="mx-5">
+          ГЛАВНАЯ
+        </a>
+        <a href="#" className="mx-5">
+          ИЗБРАННОЕ
+        </a>
+      </div>
+      <div className="mx-5">
+        <button>Вход</button>
+        <button>Регистрация</button>
+      </div>
+    </div>
   )
 }
 
